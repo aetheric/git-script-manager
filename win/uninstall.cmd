3ecef605819abab99ef81b3c7f2292b125fdc0d8
pcummuskey <pcummuskey@bravurasolutions.com>
2015-10-16 14:15:14 +1300

    Fixed windows (un)install scripts. Closes #7.

--- win/uninstall.cmd
+++ win/uninstall.cmd
@@ -1,7 +1,5 @@
 @ECHO OFF
 
-SET binpath=%~dp0bin
-
-SET pswrap="& {Start-Process PowerShell -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File ""./uninstall.ps1""' -Verb RunAs}"
+SET pswrap="& {Start-Process PowerShell -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File ""%~dp0/uninstall.ps1""' -Verb RunAs}"
 powershell -NoProfile -ExecutionPolicy Bypass -Command %pswrap%
 
